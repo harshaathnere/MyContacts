32913b57adedb5ba23c2f9aed5dbf610a5248110
harshaathnere <harshaathnere@gmail.com>
2019-03-14 16:41:00 +0530

    My very first commit

---
+++ dom.js
@@ -0,0 +1,26 @@
+//get input element
+let filterInput = document.getElementById('filterInput');
+//Add event Listner
+filterInput.addEventListener('keyup',filterNames);
+function filterNames(){
+    //get value of input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    // get names ul 
+    let ul = document.getElementById('names');
+    //get lis from ul
+
+    let li = ul.querySelectorAll('li.collection-item');
+    // loop through collection-item
+    for(let i=0; i<li.length;i++){
+      let a = li[i].getElementsByTagName('a')[0];
+      //if matched
+      if(a.innerHTML.toUpperCase().indexOf(filterValue) > 
+      -1)
+      {
+          li[i].style.display = '';
+      } else{
+        li[i].style.display = 'none';
+      }
+    }
+
+}
